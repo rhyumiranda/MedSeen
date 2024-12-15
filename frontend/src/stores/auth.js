688a840902f9accde144e35ec043c3cfee5c05a6
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-12-15 11:45:45 +0800

    feat: profile route

--- frontend/src/stores/auth.js
+++ frontend/src/stores/auth.js
@@ -4,9 +4,12 @@ export const user = writable(null);
 export const isAuthenticated = writable(false);
 
 export const setUser = (data) => {
+    console.log('Setting user:', data);
     user.set(data);
     isAuthenticated.set(!!data);
+    console.log('isAuthenticated:', !!data);
 };
+
 
 export const clearUser = () => {
     user.set(null);
